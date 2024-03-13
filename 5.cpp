ba37bf4c193d572c36331e2fb0fc9df6964ecdd9
Mabuoch222 <150212856+Mabuoch222@users.noreply.github.com>
2024-03-14 04:42:45 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+  queue<string> animals;
+
+  animals.push("Cat");
+  animals.push("Dog");
+  
+  cout << "Queue: ";
+
+  while(!animals.empty()) {
+
+    cout << animals.front() << ", ";
+
+    animals.pop();
+  }
+
+  cout << endl;
+ 
+  return 0;
+}
